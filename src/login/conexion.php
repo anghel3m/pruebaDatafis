11eee5a64f6bd8aedf7c30127c17fb1fb0930cc4
Anghel3m <adms95@outlook.es>
2024-02-27 11:27:22 -0500

    conexion y salida de la sesion

---
+++ src/login/conexion.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+session_start();
+
+//credenciales de acceso a la base datos
+$servidor = "localhost";
+$usuario = "anghel3m";
+$password = "123";
+
+
+try {
+    $conexion =  new PDO("mysql:host=$servidor;dbname=usuario", $usuario, $password);
+    $conexion -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "SELECT * FROM `fotos`";
+
+
+
+    echo "conexion establecida";
+
+} catch (PDOException $error) {
+
+    echo "conexion erronea <br>" . $error;
+}
+
+?>
