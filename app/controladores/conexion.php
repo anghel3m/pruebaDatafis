57e33a61821ff13b3df8836b5b2578a9024a4435
NESTOR ANDRES VILLAFAÑE MASS <villafanenestor@coruniamericana.edu.co>
2024-02-27 13:49:08 -0500

    se mueve controlador db

---
+++ app/controladores/conexion.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+//credenciales de acceso a la base datos
+$servidor = "localhost";
+$usuario = "anghel3m";
+$password = "123";
+
+
+try {
+    $conexion =  new PDO("mysql:host=$servidor;dbname=usuario", $usuario, $password);
+    $conexion -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+
+
+    echo "conexion establecida";
+
+} catch (PDOException $error) {
+
+    echo "conexion erronea <br>" . $error;
+}
+
+?>
