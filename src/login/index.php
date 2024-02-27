28332e52e593885db061107c13e1c4d7218fdb09
Anghel3m <adms95@outlook.es>
2024-02-27 13:18:39 -0500

    combio a bootstrao

--- src/login/index.php
+++ src/login/index.php
@@ -1,24 +1,36 @@
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/pruebaDatafis/src/login/style.css">
-    <title>Document</title>
-</head>
-<body>
-<main>
-    <div class="login">
-    <form action="conexion.php" method="post">
-        <h1>Iniciar sesion</h1>
-        <p>Correo</p> 
-        <input type="email" name="email" placeholder="Correo" id="email" required>
-        <br/>
-        <p>Contraseña</p>
-        <input type="password" name="password"placeholder="Contraseña" id="password" required>
-        <br/><br/>
-        <input type="submit" name="iniciar" value="Iniciar">
-    </div>
-</main>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <title>Bootstrap Example</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  </head>
+  <body class="p-3 m-0 border-0 bd-example m-0 border-0">
+
+    <!-- Example Code -->
+    
+        
+    <form>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Correo</label>
+        <input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp">
+        
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+      </div>
+      
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Recordarme</label>
+      </div>
+      <button type="submit" class="btn btn-primary">Iniciar</button>
     </form>
-</body>
+    
+      
+  </body>
 </html>
