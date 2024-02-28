ebef8844236a55b4d7315d0662cd4b84d2b95e06
NESTOR ANDRES VILLAFAÑE MASS <villafanenestor@coruniamericana.edu.co>
2024-02-28 10:38:41 -0500

    editar y guardado de tareas ok

--- app/tareas/index.php
+++ app/tareas/index.php
@@ -20,21 +20,26 @@ if(!$hasLogin){
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script>
-      $( document ).ready(()=>{
+    let idTarea = "";
+    $(document).ready(() => {
         consultarTareas();
-        
-      });
 
-      const rellenar = (tarea)=>{
-        const {contenido, titulo, id} = tarea;
+    });
+
+    const rellenar = (tarea) => {
+        const {
+            contenido,
+            titulo,
+            id
+        } = tarea;
         const misCards = $('#misCards');
-          misCards.append(`<div class="col">
+        misCards.append(`<div class="col p-2">
                 <div class="p-3 border bg-light">
                     <div class="card" style="width: 18rem;">
                         <div class="card-body">
                             <h5 class="card-title">${titulo}</h5>
                             <p class="card-text">${contenido}</p>
-                            <a href="#" onclick="editarTarea(${id})" class="btn btn-warning"><i class="fa fa-pencil"></i></a>
+                            <a href="#" onclick="mostrarModal('editar','${id}','${titulo}', '${contenido}')" class="btn btn-warning"><i class="fa fa-pencil"></i></a>
                             <a href="#" onclick="eliminarTarea(${id})" class="btn btn-danger"><i class="fa fa-trash"></i></a>
                         </div>
                     </div>
@@ -43,40 +48,178 @@ if(!$hasLogin){
     }
 
 
-    const eliminarTarea = (id)=>{
-      console.log(id);
+    const eliminarTarea = (id) => {
+        console.log(id);
+        $.ajax({
+            type: "POST",
+            url: "tareasController.php",
+            data: {
+                option: 'eliminarTarea',
+                id
+            },
+            success: function(response) {
+                console.log(response);
+                consultarTareas();
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
     }
-    const editarTarea = (id)=>{
-      console.log(id);
+    const editarTarea = () => {
+        const titulo = $("#titulotarea").val();
+        const contenido = $("#contenidoTarea").val();
+        $.ajax({
+            type: "POST",
+            url: "tareasController.php",
+            data: {
+                option: 'editarTarea',
+                id: idTarea,
+                titulo,
+                contenido
+            },
+            success: function(response) {
+              idTarea = "";
+                console.log(response);
+                const {status, mensaje} = response;
+                $('#exampleModalCenter').modal('hide');
+                consultarTareas();
+                mostrarAlerta({
+                  title: "Tarea editada",
+                  text: mensaje,
+                  icon: status,
+                  textButton: "Aceptar"
+                })
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
     }
 
 
-    const consultarTareas = ()=>{
-      $.ajax({
-        type: "POST",
-        url: "tareasController.php",
-        data: {option: 'consultarTareas'},
-        success: function (response) {
-          $("#misCards").html('');
-          console.log(response);
-          const values = Object.values(response);
-          console.log(values);
-          values.forEach(tarea => {
-            console.log(tarea);
-            rellenar(tarea);
-          });
-        },
-        error: function (error) {
-          console.log(error);
-        }
-      });
+    const consultarTareas = () => {
+        $.ajax({
+            type: "POST",
+            url: "tareasController.php",
+            data: {
+                option: 'consultarTareas'
+            },
+            success: function(response) {
+                $("#misCards").html('');
+                console.log(response);
+                const values = Object.values(response);
+                console.log(values);
+                values.forEach(tarea => {
+                    console.log(tarea);
+                    rellenar(tarea);
+                });
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
     }
 
 
+    const mostrarModal = (opcion, id="", titulo="", contenido="") => {
+      console.log(opcion);
+      $("#btnGuardarTarea").hide();
+      $("#btnEditarTarea").hide();
+      $('#exampleModalCenter').modal('show'); 
+      
+      if(opcion=="editar"){
+        idTarea = id;
+        $("#titulotarea").val(titulo);
+        $("#contenidoTarea").val(contenido);
+        $("#btnEditarTarea").show();
+      }else if(opcion=="nuevaTarea"){
+        $("#btnGuardarTarea").show();
+        $("#titulotarea").val("");
+        $("#contenidoTarea").val("");
+      }
+    }
+
+
+    const mostrarAlerta = ({title, text, icon, textButton})=>{
+      Swal.fire({
+        title: title,
+        text: text,
+        icon: icon,
+        confirmButtonText: textButton
+      })
+    }
+
+
+    $(document).on('click', 'button[name="closeModal"]', function() {
+        $('#exampleModalCenter').modal('hide');
+    });
+
+
+    const guardarTarea = () => {
+        const titulo = $("#titulotarea").val();
+        const contenido = $("#contenidoTarea").val();
+        $.ajax({
+            type: "POST",
+            url: "tareasController.php",
+            data: {
+                option: 'guardarTarea',
+                titulo,
+                contenido
+            },
+            success: function(response) {
+                console.log(response);
+                const {status, mensaje} = response;
+                $('#exampleModalCenter').modal('hide');
+                consultarTareas();
+                mostrarAlerta({
+                  title: "Tarea guardada",
+                  text: mensaje,
+                  icon: status,
+                  textButton: "Aceptar"
+                })
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+    }
     </script>
 </head>
 
 <body>
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog"
+        aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalTitle">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close" name="closeModal">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                <div class="form-group">
+                  <label for="titulotarea" class="col-form-label">Titulo Tarea:</label>
+                  <input type="text" class="form-control" id="titulotarea">
+                </div>
+                <div class="form-group">
+                  <label for="contenidoTarea" class="col-form-label">Contenido Tarea:</label>
+                  <input type="text" class="form-control" id="contenidoTarea">
+                </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-toggle="modal" name="closeModal">Close</button>
+                    <button type="button" class="btn btn-primary" onclick="guardarTarea()" id="btnGuardarTarea" >Guardar tarea</button>
+                    <button type="button" class="btn btn-primary" onclick="editarTarea() " id="btnEditarTarea">Editar tarea</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Modal -->
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">Navbar</a>
@@ -100,10 +243,10 @@ if(!$hasLogin){
     <div class="container">
         <div class="row">
             <div class="col">
-                <button onclick="rellenar()" class="btn btn-primary">Rellenar</button>
+                <button onclick="mostrarModal('nuevaTarea')" class="btn btn-primary">Crear Nueva Tarea</button>
             </div>
             <div class="col">
-                <button onclick="consultarTareas()" class="btn btn-primary">Consultar tareas</button>
+                <button onclick="consultarTareas()" class="btn btn-primary">Consultar Tareas Guardadas</button>
             </div>
         </div>
 
@@ -111,7 +254,7 @@ if(!$hasLogin){
 
     <div class="container">
         <div class="row row-cols-1 row-cols-lg-3 md-2" id="misCards">
-            
+
         </div>
     </div>
 
@@ -119,3 +262,6 @@ if(!$hasLogin){
 </body>
 
 </html>
+
+
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
--- app/tareas/tareasController.php
+++ app/tareas/tareasController.php
@@ -6,13 +6,19 @@ $option = isset($_POST['option']) ? $_POST['option'] : $_GET['option'];
 
 if($option == 'consultarTareas'){
     consultarTareas();
+}else if($option == 'eliminarTarea'){
+    eliminarTarea();
+}else if($option == 'editarTarea'){
+    editarTarea();
+}else if($option == 'guardarTarea'){
+    guardarTarea();
 }else{
     echo "Opción no válida";
 }
 
 function consultarTareas(){
     global $conexion;
-    $sql = "SELECT * FROM tareas";
+    $sql = "SELECT * FROM tareas order by ID desc";
     $result = $conexion->query($sql);
     $tareas = [];
     if($result->rowCount() > 0){
@@ -27,4 +33,53 @@ function consultarTareas(){
     }
     header('Content-Type: application/json');
     echo json_encode($tareas);
+}
+
+
+function eliminarTarea(){
+    global $conexion;
+    $id = $_POST['id'];
+    $sql = "DELETE FROM tareas WHERE ID = $id";
+    $result = $conexion->query($sql);
+    if($result->rowCount() > 0){
+        $response = ['status' => 'success', "mensaje" => 'Tarea eliminada'];
+    }else{
+        $response = ['status' => 'error', "mensaje" => 'No se pudo eliminar la tarea', 'status' => 'sql', "mensaje" => $sql];
+    }
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
+
+function editarTarea(){
+    global $conexion;
+    $id = $_POST['id'];
+    $titulo = $_POST['titulo'];
+    $contenido = $_POST['contenido'];
+    $sql = "UPDATE tareas SET TITULO = '$titulo', CONTENIDO = '$contenido' WHERE ID = $id";
+    $result = $conexion->query($sql);
+    if($result->rowCount() > 0){
+        $response = ['status' => 'success', "mensaje" => 'Tarea actualizada'];
+    }else{
+        $response = ['status' => 'error', "mensaje" => 'No se pudo actualizar la tarea', 'sql' => $sql];
+    }
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
+
+function guardarTarea(){
+    global $conexion;
+    $titulo = $_POST['titulo'];
+    $contenido = $_POST['contenido'];
+    $sql = "INSERT INTO tareas (TITULO, CONTENIDO) VALUES (:titulo, :contenido)";
+    $strm = $conexion->prepare($sql);
+    $strm->bindParam(':titulo', $titulo);
+    $strm->bindParam(':contenido', $contenido);
+    $strm->execute();
+    if($strm->rowCount() > 0){
+        $response = ['status' => 'success', "mensaje" => 'Tarea guardada'];
+    }else{
+        $response = ['status' => 'error', "mensaje" => 'No se pudo guardar la tarea', 'sql' => $sql];
+    }
+    header('Content-Type: application/json');
+    echo json_encode($response);
 }
