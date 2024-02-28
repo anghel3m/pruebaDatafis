dcef630ebf5f796fa0bf2364fd66adea667ee723
Anghel3m <adms95@outlook.es>
2024-02-27 21:41:28 -0500

    botones y cambio de tabla

--- app/tareas/index.php
+++ app/tareas/index.php
@@ -40,35 +40,30 @@ if(!$hasLogin){
 </nav>
 
 
+
+<div>
+
 <table class="table">
   <thead>
     <tr>
       <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Tarea</th>
     </tr>
   </thead>
+  
   <tbody>
+
     <tr>
       <th scope="row">1</th>
       <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
     </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
+    
+    <button type="button" class="btn btn-success">agregar</button>
+    <button type="button" class="btn btn-danger">eliminar</button>
+    <button type="button" class="btn btn-warning">modificar</button>
   </tbody>
 </table>
+</div>
 
 </body>
 </html>
